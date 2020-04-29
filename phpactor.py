88fe2e184b4baaf0bf43ca127136374c5343454a
tkotosz <tkotosz@inviqa.com>
2020-04-29 20:10:14 +0200

    implement class_inflect

--- phpactor.py
+++ phpactor.py
@@ -337,6 +337,16 @@ class PhpactorOffsetInfoCommand(sublime_plugin.TextCommand):
         }
         self.view.run_command('phpactor_rpc', request)
 
+class PhpactorClassInflectCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        request = {
+            'action': 'class_inflect',
+            'parameters': {
+                'current_path': '@current_path'
+            }
+        }
+        self.view.run_command('phpactor_rpc', request)
+
 class PhpactorClassNewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         request = {
@@ -464,7 +474,7 @@ class PhpactorEditorActionInputCallbackCommand(sublime_plugin.TextCommand):
 
         if input['type'] == 'choice':
             items = []
-            
+
             for item in input['parameters']['choices']:
                 items.append(item)
 
