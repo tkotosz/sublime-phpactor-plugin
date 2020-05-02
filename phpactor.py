e7f4827a5d19cb44b90f0b810ed2e701c9857d91
tkotosz <tkotosz@inviqa.com>
2020-05-02 10:07:55 +0200

    add extract_method command

--- phpactor.py
+++ phpactor.py
@@ -467,6 +467,19 @@ class PhpactorExtractExpressionCommand(sublime_plugin.TextCommand):
         }
         self.view.run_command('phpactor_rpc', request)
 
+class PhpactorExtractMethodCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        request = {
+            'action': 'extract_method',
+            'parameters': {
+                'source': '@current_source',
+                'path': '@current_path',
+                'offset_start': '@current_offset_start',
+                'offset_end': '@current_offset_end'
+            }
+        }
+        self.view.run_command('phpactor_rpc', request)
+
 class PhpactorClassSearchCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         keyword = self.get_current_word()
