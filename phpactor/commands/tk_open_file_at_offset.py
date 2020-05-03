86726ffd5c548b964e20ff62b4bf7ae7ab8f2805
tkotosz <tkotosz@inviqa.com>
2020-05-03 17:19:28 +0200

    simplify open file & jump to offset

--- phpactor/commands/tk_open_file_at_offset.py
+++ phpactor/commands/tk_open_file_at_offset.py
@@ -3,18 +3,14 @@ import sublime_plugin
 
 class TkOpenFileAtOffsetCommand(sublime_plugin.TextCommand):
     def run(self, edit, file_path, offset):
-        if self.view.file_name() != file_path:
-            self.view.window().open_file(file_path, sublime.ENCODED_POSITION | sublime.FORCE_GROUP, self.view.window().active_group())
-            self.view.window().run_command(
-                'tk_run_command_when_file_loaded',
-                {
-                    'file_path': file_path,
-                    'command_name': 'tk_open_file_at_offset',
-                    'command_params': { 'file_path': file_path, 'offset': offset }
-                }
-            );
-            return
+        self.view.window().open_file(file_path, sublime.ENCODED_POSITION | sublime.FORCE_GROUP, self.view.window().active_group())
+        sublime.set_timeout_async(lambda: self.jump_to_offset(file_path, offset))
 
-        self.view.sel().clear()
-        self.view.sel().add(sublime.Region(offset))
-        sublime.set_timeout(lambda: self.view.show_at_center(offset), 0)
+    def jump_to_offset(self, file_path, offset):
+        file_view = self.view.window().find_open_file(file_path)
+        region = sublime.Region(offset, offset)
+        self.view.window().focus_view(file_view)
+        file_view.sel().clear()
+        file_view.sel().add(region)
+        file_view.show_at_center(region)
+        
