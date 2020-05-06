5634f7fd18adfcc2eddecd9e0bc68a2ce70309b6
tkotosz <tkotosz@inviqa.com>
2020-05-06 10:40:15 +0200

    bugfix: fix open file editor action for relative path

--- modules/commands/phpactor_rpc/editor_action/open_file.py
+++ modules/commands/phpactor_rpc/editor_action/open_file.py
@@ -1,10 +1,11 @@
 import sublime
 import sublime_plugin
+from ....utils import *
 
 class PhpactorEditorActionOpenFileCommand(sublime_plugin.TextCommand):
     def run(self, edit, force_reload, path, offset, target):
         # navigate command returns relative path for some reason
         if path.find('/') != 0:
-            path = tksublime.find_working_dir(view) + '/' + path
+            path = find_working_dir(self.view) + '/' + path
 
         self.view.run_command('tk_open_file_at_offset', { 'file_path': path, 'offset': offset } )
